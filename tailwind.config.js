6169c46f6af36e470d20230a2a93ec250e96ba46
rajib074 <rajib01711173241@gmail.com>
2022-05-30 18:34:17 +0600

    Update tailwind update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,35 @@
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "light-primary": "#ffe7a5",
+      },
+    },
   },
-  plugins: [require("daisyui")],
-}
+
+  daisyui: {
+    themes: [
+      {
+        azo: {
+          "primary": "#FFBA00",
+          "secondary": "#1d233a",
+          "accent": "#1d233a",
+          "neutral": "#c5f617",
+          "base-100": "#FFFFFF",
+          "info": "#4a4f61",
+          "success": "#36D399",
+          "warning": "#FBBD23",
+          "error": "#F87272",
+        },
+      },
+    ],
+  },
+  
+  plugins: [
+    require('daisyui'),
+  ],
+} 
