b1a3505a7f94510f9f8dba800b152c9e2bdbfae8
rajib074 <rajib01711173241@gmail.com>
2022-05-30 18:39:25 +0600

    sugn up

---
+++ src/Pages/Products/Products.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ProductItem from '../../Shared/ProductItem';
+
+const Products = () => {
+    return (
+        <>
+            <section className='py-28'>
+                <ProductItem />
+            </section>
+        </>
+    );
+};
+
+export default Products;
